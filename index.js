92a9d774cb5c161cd84a977ddcd4412bfce9172c
Your Name <yamacarkan@seattleacademy.org>
2020-02-05 11:13:03 -0800

    add stuff

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+const port = 1515
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => res.send('Hello Yamac!'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send('Counter ' + counter)
+})
+app.get('/date', function (req, res) {
+	let d = new Date ();
+  res.send('Date' + d)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))	
